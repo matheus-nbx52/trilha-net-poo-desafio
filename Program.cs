42500c8995913143978e75171e57a38f03cc4f7f
Matheus Francisco <matheusfssilva2001@gmail.com>
2023-12-21 00:47:52 -0300

    arrumando algumas coisas

--- Program.cs
+++ Program.cs
@@ -7,21 +7,20 @@ namespace DesafioPOO
     {
         static void Main(string[] args)
         {
-            // Criando uma instância de Nokia
             Nokia nokiaPhone = new Nokia("856458789", "Nokia Model", "12948", 51);
 
-            // Testando métodos do Nokia
             nokiaPhone.Ligar();
             nokiaPhone.ReceberLigacao();
-            nokiaPhone.InstalarAplicativo("WhatsApp");
+            nokiaPhone.InstalarAplicativo("Facebook");
 
-            // Criando uma instância de Iphone
+            Console.WriteLine("\n");
+
+            
             Iphone iphone = new Iphone("987657781", "iPhone Model", "50301", 198);
 
-            // Testando métodos do iPhone
             iphone.Ligar();
             iphone.ReceberLigacao();
-            iphone.InstalarAplicativo("Instagram");
+            iphone.InstalarAplicativo("Minecraft");
 
             Console.ReadLine();
         }
